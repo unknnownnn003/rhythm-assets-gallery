43e0afa23b3d34354cd67623a717c9c4f31bd3de
Unknnownnn <3057887752@qq.com>
2026-06-20 13:30:16 +0800

    Fix homepage hydration CSP

--- src/lib/security.ts
+++ src/lib/security.ts
@@ -1,7 +1,7 @@
 export const SITE_CSP = [
   "default-src 'self'",
   "img-src 'self' data: blob:",
-  "script-src 'self'",
+  "script-src 'self' 'unsafe-inline'",
   "style-src 'self' 'unsafe-inline'",
   "connect-src 'self' http://localhost:3001",
   "frame-ancestors 'none'",
